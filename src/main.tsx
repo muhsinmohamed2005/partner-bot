96bc72342215e2da54126c31dcbe9ad123b3bba0
Muhsin Mohamed <muhsinmohamed05@Muhsins-MacBook-Pro-6.local>
2026-07-20 19:17:11 -0500

    Amplify Gen 2 setup: Vite + React + TS scaffold, empty backend, sandbox deployed

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+
+Amplify.configure(outputs);
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
